80d95f3838921da9f5216b9fe8bd471fe72431e5
niranjan-prathap-tj <niranjan.prathap@ideas2it.com>
2022-07-05 17:51:36 +0530

    CORS

--- demo/backend/src/main/java/com/example/demo/Message.java
+++ demo/backend/src/main/java/com/example/demo/Message.java
@@ -8,12 +8,15 @@ public class Message {
     @Id
     private String message;
     Message() {}
+
     Message(String message) {
         this.message = message;
     }
+
     public String getMessage() {
         return message;
     }
+    
     public void setMessage(String message) {
         this.message = message;
     }
